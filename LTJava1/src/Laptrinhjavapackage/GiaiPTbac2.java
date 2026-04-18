2f03f9eb99fb106e28e3d3eb488e470efa60a955
unknown <ttanh12a22324@gmail.com>
2026-04-18 11:30:19 +0700

    Giai PT bac 2

--- LTJava1/src/Laptrinhjavapackage/GiaiPTbac2.java
+++ LTJava1/src/Laptrinhjavapackage/GiaiPTbac2.java
@@ -1,10 +1,47 @@
 package Laptrinhjavapackage;
 
+import java.util.Scanner;
+
 public class GiaiPTbac2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-}
+        System.out.print("Nhập a: ");
+        double a = sc.nextDouble();
+        System.out.print("Nhập b: ");
+        double b = sc.nextDouble();
+        System.out.print("Nhập c: ");
+        double c = sc.nextDouble();
+
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println("PTVSN (Phương trình vô số nghiệm)");
+                } else {
+                    System.out.println("PTVN (Phương trình vô nghiệm)");
+                }
+            } else {
+                double x = -c / b;
+                System.out.println("Phương trình bậc 1 có nghiệm: x = " + x);
+            }
+        } else {
+            double delta = b * b - 4 * a * c;
+
+            if (delta < 0) {
+                System.out.println("PTVN (Phương trình vô nghiệm)");
+            } else if (delta == 0) {
+                double x = -b / (2 * a);
+                System.out.println("Phương trình có nghiệm kép: x = " + x);
+            } else {
+                double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("Phương trình có 2 nghiệm phân biệt:");
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
+        }
+
+        sc.close();
+    } 
+} 
