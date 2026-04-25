f4de6cd09f0b43c0010b10c5890afc9178e961ea
ttanh12a22324-rgb <ttanh12a22324@gmail.com>
2026-04-25 09:56:44 +0700

    Update baitapmang.java

--- src/Laptrinhjavapackage/baitapmang.java
+++ src/Laptrinhjavapackage/baitapmang.java
@@ -1,20 +1,60 @@
+package Laptrinhjavapackage;
 import java.util.Scanner;
-import java.util.Arrays;
 import java.util.Random;
 
-public class BaiTapMang {
+public class baitapmang {
+
+    static void xuatMang(int[] arr) {
+        for (int x : arr) System.out.print(x + " ");
+        System.out.println();
+    }
+
+    static int tinhTong(int[] arr) {
+        int tong = 0;
+        for (int x : arr) tong += x;
+        return tong;
+    }
+
+    static int countK(int[] arr, int k) {
+        int dem = 0;
+        for (int x : arr) if (x == k) dem++;
+        return dem;
+    }
+
+    static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int x : arr) if (x > max) max = x;
+        return max;
+    }
+
+    static int findMin(int[] arr) {
+        int min = arr[0];
+        for (int x : arr) if (x < min) min = x;
+        return min;
+    }
+
+    static boolean laNguyenTo(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+
+    static void xuatSoNguyenTo(int[] arr) {
+        for (int x : arr) if (laNguyenTo(x)) System.out.print(x + " ");
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Nhập số lượng phần tử N: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        
-        
+
         Random rd = new Random();
         for (int i = 0; i < n; i++) {
-            arr[i] = rd.nextInt(100); 
+            arr[i] = rd.nextInt(100);
         }
 
         System.out.println("\n--- Toàn bộ mảng ---");
@@ -32,69 +72,6 @@ public class BaiTapMang {
         System.out.print("Các số nguyên tố trong mảng: ");
         xuatSoNguyenTo(arr);
 
-        int[] arrTang = arr.clone();
-        Arrays.sort(arrTang);
-        System.out.print("\n\nMảng sau khi sắp xếp tăng dần: ");
-        xuatMang(arrTang);
-
-        System.out.print("Mảng sau khi sắp xếp giảm dần: ");
-        sapXepGiam(arr);
-        xuatMang(arr);
-    }
-
-    public static void xuatMang(int[] a) {
-        for (int x : a) System.out.print(x + " ");
-        System.out.println();
-    }
-
-    public static int tinhTong(int[] a) {
-        int s = 0;
-        for (int x : a) s += x;
-        return s;
-    }
-
-    public static int countK(int[] a, int k) {
-        int count = 0;
-        for (int x : a) if (x == k) count++;
-        return count;
-    }
-
-    public static int findMax(int[] a) {
-        int max = a[0];
-        for (int x : a) if (x > max) max = x;
-        return max;
-    }
-
-    public static int findMin(int[] a) {
-        int min = a[0];
-        for (int x : a) if (x < min) min = x;
-        return min;
-    }
-
-    public static boolean isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
-    }
-
-    public static void xuatSoNguyenTo(int[] a) {
-        for (int x : a) {
-            if (isPrime(x)) System.out.print(x + " ");
-        }
-    }
-
-    public static void sapXepGiam(int[] a) {
-        
-        for (int i = 0; i < a.length - 1; i++) {
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[i] < a[j]) {
-                    int temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                }
-            }
-        }
+        sc.close();
     }
 }
